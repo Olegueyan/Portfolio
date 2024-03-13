d17c1b6cd81502c469b201a5670bfbfec2004315
Olegueyan <gsourtg2cer@gmail.com>
2024-03-13 10:53:42 +0100

    React Component : Footer UP2

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -2,8 +2,18 @@ function Footer()
 {
     return (
         <div className="Footer">
+            <div className="Footer_email">
+                <img className="Footer_email_icon" src="src/assets/icons/e-mail.png" alt="Email" />
+                <span className="Footer_email_button">Envoyer un mail</span>
+            </div>
+
             <div className="Footer_copyright">
                 © 2024 - Gaëtan Rousselin
+            </div>
+
+            <div className="Footer_developped">
+                <img className="Footer_developped_icon" src="src/assets/icons/reactjs.png" alt="ReactJS" />
+                <span className="Footer_developped_text">Developed in ReactJS</span>
             </div>
         </div>
     )
