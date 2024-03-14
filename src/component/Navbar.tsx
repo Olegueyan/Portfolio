6aa771d57d5abcb75f831c876afa66e6162cdf69
Olegueyan <gsourtg2cer@gmail.com>
2024-03-14 15:41:06 +0100

    Navbar update

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -17,17 +17,19 @@ function Navbar()
 
     return (
         <div className="Navbar">
-            <div className="Navbar_profil">
-                <img className="Navbar_profil_image" src="src/assets/images/profil.png" alt="Gaëtan Rousselin" />
-                <span className="Navbar_profil_name">Gaëtan Rousselin</span>
-            </div>
-            <div className="Navbar_socialNetworks">
-                <img className="Navbar_socialNetworks_linkedin Navbar_socialNetworks__socialIcon"
-                     src="src/assets/icons/linkedin.png" alt="Linkedin" onClick={onLinkedinClick}/>
-                <img className="Navbar_socialNetworks_discord Navbar_socialNetworks__socialIcon"
-                     src="src/assets/icons/discord.png" alt="Discord" onClick={onDiscordClick}/>
-                <img className="Navbar_socialNetworks_github Navbar_socialNetworks__socialIcon"
-                     src="src/assets/icons/github.png" alt="Github" onClick={onGithubClick}/>
+            <div className="Navbar_main">
+                <div className="Navbar_main_profil">
+                    <img className="Navbar_main_profil_image" src="src/assets/images/profil.png" alt="Gaëtan Rousselin" />
+                    <span className="Navbar_main_profil_name">Gaëtan Rousselin</span>
+                </div>
+                <div className="Navbar_main_socialNetworks">
+                    <img className="Navbar_main_socialNetworks_linkedin Navbar_main_socialNetworks__socialIcon"
+                         src="src/assets/icons/linkedin.png" alt="Linkedin" onClick={onLinkedinClick}/>
+                    <img className="Navbar_main_socialNetworks_discord Navbar_main_socialNetworks__socialIcon"
+                         src="src/assets/icons/discord.png" alt="Discord" onClick={onDiscordClick}/>
+                    <img className="Navbar_main_socialNetworks_github Navbar_main_socialNetworks__socialIcon"
+                         src="src/assets/icons/github.png" alt="Github" onClick={onGithubClick}/>
+                </div>
             </div>
         </div>
     )
