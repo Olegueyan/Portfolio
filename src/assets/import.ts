f745cdd2650339ce00bd68964404d854fcd65f4a
Olegueyan <gsourtg2cer@gmail.com>
2024-03-19 14:44:24 +0100

    Creation of assets bundler

---
+++ src/assets/import.ts
@@ -0,0 +1,179 @@
+// ----- Assets imports | ICON ----- //
+
+import imgCareer from "./images/icon/career.png";
+import imgDownload from "./images/icon/download.png";
+import imgEmail from "./images/icon/e-mail.png";
+import imgFormations from "./images/icon/formations.png";
+import imgHome from "./images/icon/home.png";
+import imgLoup from "./images/icon/loup.png";
+import imgProfile from "./images/icon/profile.png";
+import imgProject from "./images/icon/project.png";
+import imgSkills from "./images/icon/skills.png";
+
+export
+{
+  imgCareer,
+  imgDownload,
+  imgEmail,
+  imgFormations,
+  imgHome,
+  imgLoup,
+  imgProfile,
+  imgProject,
+  imgSkills
+}
+
+// --------------------------------- //
+
+// ----- Assets imports | LANG ----- //
+
+import imgCSharp from "./images/lang/csharp.png";
+import imgCSS from "./images/lang/css-3.png";
+import imgHTML from "./images/lang/html-5.png";
+import imgJava from "./images/lang/java.png";
+import imgJavascript from "./images/lang/javascript.png";
+import imgPHP from "./images/lang/php.png";
+import imgPython from "./images/lang/python.png";
+import imgReactJS from "./images/lang/reactJS.png";
+import imgRust from "./images/lang/rust.png";
+import imgSass from "./images/lang/sass.png";
+import imgSQL from "./images/lang/sql.png";
+import imgSymfony from "./images/lang/symfony.png";
+import imgTypescript from "./images/lang/typescript.png";
+import imgViteJS from "./images/lang/vitejs.png";
+
+export
+{
+  imgCSharp,
+  imgCSS,
+  imgHTML,
+  imgJava,
+  imgJavascript,
+  imgPHP,
+  imgPython,
+  imgReactJS,
+  imgRust,
+  imgSass,
+  imgSQL,
+  imgSymfony,
+  imgTypescript,
+  imgViteJS
+}
+
+// --------------------------------- //
+
+// ----- Assets imports | LIB ----- //
+
+import imgWinForms from "./images/lib/winforms.png";
+
+export
+{
+  imgWinForms
+}
+
+// -------------------------------- //
+
+// ----- Assets imports | MEDIA ----- //
+
+import imgDiscordMedia from "./images/media/discordMedia.png";
+import imgGithubMedia from "./images/media/githubMedia.png";
+import imgLinkedinMedia from "./images/media/linkedinMedia.png";
+
+export
+{
+  imgDiscordMedia,
+  imgGithubMedia,
+  imgLinkedinMedia
+}
+
+// ---------------------------------- //
+
+// ----- Assets imports | PROJECT ----- //
+
+import imgMenagelec from "./images/project/menagelec/menagelec.png";
+import imgMenagelecSlideOne from "./images/project/menagelec/slide_menagelec1.png";
+import imgMenagelecSlideTwo from "./images/project/menagelec/slide_menagelec2.png";
+import imgMenagelecSlideThree from "./images/project/menagelec/slide_menagelec3.png";
+import imgMenagelecSlideFour from "./images/project/menagelec/slide_menagelec4.png";
+import imgMenagelecSlideFive from "./images/project/menagelec/slide_menagelec5.png";
+
+import imgMonstroclasse from "./images/project/monstroclasse.gif";
+import imgTelem from "./images/project/telem.png";
+
+export
+{
+  imgMenagelec,
+  imgMenagelecSlideOne,
+  imgMenagelecSlideTwo,
+  imgMenagelecSlideThree,
+  imgMenagelecSlideFour,
+  imgMenagelecSlideFive,
+  imgMonstroclasse,
+  imgTelem
+}
+
+// ------------------------------------ //
+
+// ----- Assets imports | SERVICE ----- //
+
+import imgDocker from "./images/service/docker.png";
+import imgGithub from "./images/service/github.png";
+import imgMysql from "./images/service/mysql.png";
+
+export
+{
+  imgDocker,
+  imgGithub,
+  imgMysql
+}
+
+// ------------------------------------ //
+
+// ----- Assets imports | SOFTWARE ----- //
+
+import imgDataGrip from "./images/software/datagrip.ico";
+import imgIdea from "./images/software/idea.ico";
+import imgPhpStorm from "./images/software/phpstorm.ico";
+import imgRider from "./images/software/rider.ico";
+import imgRustRover from "./images/software/rustrover.ico";
+import imgSqliteBrowser from "./images/software/sqlitebrowser.svg";
+import imgVSCode from "./images/software/vscode.svg";
+import imgWebStorm from "./images/software/webstorm.ico";
+
+export
+{
+  imgDataGrip,
+  imgIdea,
+  imgPhpStorm,
+  imgRider,
+  imgRustRover,
+  imgSqliteBrowser,
+  imgVSCode,
+  imgWebStorm
+}
+
+// ------------------------------------ //
+
+// ----- Assets imports | TOOL ----- //
+
+import imgGit from "./images/tool/git.png";
+import imgNodeJS from "./images/tool/nodejs.png";
+
+export
+{
+  imgGit,
+  imgNodeJS
+}
+
+// --------------------------------- //
+
+// ----- Assets imports | ROOT ----- //
+
+import imgProfileHead from "./images/profileHead.png";
+
+export
+{
+  imgProfileHead
+}
+
+// --------------------------------- //
